817d0e549b94daa93ded285cbf832d860f632b93
egordina <jgarm@yandex.ru>
2016-02-20 14:21:16 +0300

    gitignore for dbConnection

--- app/components/blog/postController.js
+++ app/components/blog/postController.js
@@ -1,36 +1,38 @@
 /**
  * Created by jokerwolf on 26/12/15.
  */
-blogModule.controller('postCtrl',['$scope', '$sce', 'Posts', function($scope, $sce, Posts){
+blogModule.controller('postCtrl',['$scope', '$sce', 'Posts', function($scope, $sce, Posts) {
     var socket = io('http://localhost:8081');
     socket.on('commentsReady', function (data) {
-        $scope.posts.filter((p) => { return p.id == data.postId})[0].comments = data.comments;
+        $scope.posts.filter((p) => {
+            return p.id == data.postId
+        })[0].comments = data.comments;
     });
-        Posts.get(null, function(posts){
-            $scope.posts = posts.map(function(element){
-                return postFromJson(element);
-            });
+    Posts.get(null, function (posts) {
+        $scope.posts = posts.map(function (element) {
+            return postFromJson(element);
         });
+    });
 
-        $scope.likePost = function(index){
-            //TODO change to specific like add/remove
-            if(!$scope.posts[index].isLiked){
-                $scope.posts[index].likes.push({});
-                $scope.posts[index].isLiked = true;
-            } else {
-                $scope.posts[index].likes.pop();
-                $scope.posts[index].isLiked = false;
-            }
-        };
-        $scope.commentPost = function(index){
-            $scope.posts[index].isCommenting = !$scope.posts[index].isCommenting;
-        };
-
-        function postFromJson(jsonData){
-            return new Post(jsonData.id, jsonData.title, jsonData.postDate, $sce.trustAsHtml(jsonData.content),
-                jsonData.likes, jsonData.comments.map(function(c){
-                    c.text = $sce.trustAsHtml(c.text);
-                    return c;
-                }));
+    $scope.likePost = function (index) {
+        //TODO change to specific like add/remove
+        if (!$scope.posts[index].isLiked) {
+            $scope.posts[index].likes.push({});
+            $scope.posts[index].isLiked = true;
+        } else {
+            $scope.posts[index].likes.pop();
+            $scope.posts[index].isLiked = false;
         }
-    }]);
+    };
+    $scope.commentPost = function (index) {
+        $scope.posts[index].isCommenting = !$scope.posts[index].isCommenting;
+    };
+
+    function postFromJson(jsonData) {
+        return new Post(jsonData.id, jsonData.title, jsonData.postDate, $sce.trustAsHtml(jsonData.content),
+            jsonData.likes, jsonData.comments.map(function (c) {
+                c.text = $sce.trustAsHtml(c.text);
+                return c;
+            }));
+    }
+}]);
--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -7,7 +7,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'j1803poimd10li',
+    password : 'j1803poi',
     database : 'blogartist'
 });
 
