2d1f9f2dcddd70bd3315a6c2368dc228adfd0e25
egordina <jgarm@yandex.ru>
2016-02-24 19:03:08 +0300

    comments display from db

--- app/components/blog/postController.js
+++ app/components/blog/postController.js
@@ -3,15 +3,25 @@
  */
 blogModule.controller('postCtrl',['$scope', '$sce', 'Posts', function($scope, $sce, Posts) {
     var socket = io('http://localhost:8081');
+
     socket.on('commentsReady', function (data) {
         $scope.posts.filter((p) => {
             return p.id == data.postId
         })[0].comments = data.comments;
     });
+
     Posts.get(null, function (posts) {
         $scope.posts = posts.map(function (element) {
             return postFromJson(element);
         });
+    });
+
+
+    $scope.$watchCollection('data.comments', function(newComments, oldComments) {
+        if (oldComments.length != newComments.length){
+            //need to save changes
+            console.log('saving...');
+        }
     });
 
     $scope.likePost = function (index) {
