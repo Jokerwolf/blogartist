5ee80fffa1a76c8fdc6518f7030730b6bc60574c
egordina <jgarm@yandex.ru>
2016-03-13 16:53:08 +0300

    button for add comments

--- app/components/comment/commentsSectionDirective.js
+++ app/components/comment/commentsSectionDirective.js
@@ -7,6 +7,19 @@ blogModule.directive('commentsSection', ['Socket', function(Socket) {
     function commentsSectionLink($scope, element, attrs){
         $scope.newComment = {};
 
+        $scope.addNewComment = function(){
+            $scope.newComment = { post_id: $scope.data.id, content: newCommentContainer.html() };
+            $scope.data.comments.push($scope.newComment);
+
+            Socket.emit('newComment', $scope.newComment);
+
+            newCommentContainer.html('');
+            $scope.newComment = {};
+
+            newCommentContainer.blur();
+            window.getSelection().removeAllRanges();
+        };
+
         var newCommentContainer = element.find('.new-comment-container');
         newCommentContainer.on('focus', function(){
             if ($scope.newComment.content == null){
@@ -16,18 +29,7 @@ blogModule.directive('commentsSection', ['Socket', function(Socket) {
 
         newCommentContainer.on('keydown', function(e){
             if (e.ctrlKey && e.keyCode == 13) {
-                $scope.$apply(function () {
-                    $scope.newComment = { post_id: $scope.data.id, content: newCommentContainer.html() };
-                    $scope.data.comments.push($scope.newComment);
-
-                    Socket.emit('newComment', $scope.newComment);
-
-                    newCommentContainer.html('');
-                    $scope.newComment = {};
-
-                    newCommentContainer.blur();
-                    window.getSelection().removeAllRanges();
-                });
+                $scope.$apply($scope.addNewComment());
             }
         });
     }
